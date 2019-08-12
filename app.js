74f5ef6cb48d9a695d16b5321334fa2b37d54f80
AbhiY98 <abhishekyadav88846@gmail.com>
2019-08-13 00:27:40 +0530

    added start script

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get("/",function(req,res){
     res.render("index");
 });
 
-app.listen(process.env.PORT || 3000,function(){
-console.log(`server started`);
+app.listen(process.env.PORT || 3000,function() {
+    console.log(`server started`);
 });
